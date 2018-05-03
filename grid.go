0ec657c030ab0f91bdeef55ea9e25d1be2fda8e4
AlexD <alex.demskie@gmail.com>
2018-05-03 00:56:39 -0700

    implementing method to retreive multiple nearest neighbors

--- grid.go
+++ grid.go
@@ -8,30 +8,26 @@ import (
 )
 
 type Point struct {
-	ID, X, Y   int64
+	ID, X, Y int64
 }
 
 type Grid struct {
 	mtx       *sync.RWMutex
-	rows      int64
-	columns   int64
 	buckets   [][][]Point
 	allPoints map[int64]*Point
 }
 
-func NewGrid(rows, columns int64) *Grid {
-	if rows < 2 || columns < 2 {
+func NewGrid(numberOfSquares int32) *Grid {
+	if numberOfSquares < 4 {
 		return nil
 	}
 	g := &Grid{
 		mtx:       &sync.RWMutex{},
-		rows:      rows,
-		columns:   columns,
-		buckets:   make([][][]Point, rows),
-		allPoints: make(map[int64]*Point, rows*columns),
+		buckets:   make([][][]Point, int32(math.Sqrt(float64(numberOfSquares)))),
+		allPoints: make(map[int64]*Point, numberOfSquares),
 	}
 	for x := range g.buckets {
-		g.buckets[x] = make([][]Point, rows)
+		g.buckets[x] = make([][]Point, len(g.buckets))
 		for y := range g.buckets[x] {
 			g.buckets[x][y] = []Point{}
 		}
@@ -39,21 +35,24 @@ func NewGrid(rows, columns int64) *Grid {
 	return g
 }
 
-func calculateBucket(x, y, rows, columns int64) (xb, yb int64) {
-	xb, yb = columns/2, rows/2
-	xb += x / (2 * (1+(math.MaxInt64 / columns)))
-	yb += y / (2 * (1+(math.MaxInt64 / rows)))
+func calculateBucket(x, y, diameter int64) (xb, yb int64) {
+	xb, yb = diameter/2, diameter/2
+	xb += x / (2 * (1 + (math.MaxInt64 / diameter)))
+	yb += y / (2 * (1 + (math.MaxInt64 / diameter)))
 	return xb, yb
 }
 
 func (g *Grid) Add(id, x, y int64) error {
+	if id < 0 {
+		return errors.New("id is a negative number")
+	}
 	g.mtx.Lock()
 	_, exists := g.allPoints[id]
 	if exists {
 		g.mtx.Unlock()
 		return errors.New("id already exists")
 	}
-	xb, yb := calculateBucket(x, y, g.rows, g.columns)
+	xb, yb := calculateBucket(x, y, int64(len(g.buckets)))
 	newPoint := Point{id, x, y}
 	g.buckets[xb][yb] = append(g.buckets[xb][yb], newPoint)
 	g.allPoints[id] = &newPoint
@@ -72,8 +71,8 @@ func (g *Grid) Move(id, x, y int64) error {
 		g.mtx.Unlock()
 		return nil
 	}
-	xb1, yb1 := calculateBucket(point.X, point.Y, g.rows, g.columns)
-	xb2, yb2 := calculateBucket(x, y, g.rows, g.columns)
+	xb1, yb1 := calculateBucket(point.X, point.Y, int64(len(g.buckets)))
+	xb2, yb2 := calculateBucket(x, y, int64(len(g.buckets)))
 	if xb1 != xb2 || yb1 != yb2 {
 		for i := range g.buckets[xb1][yb1] {
 			if g.buckets[xb1][yb1][i].ID == point.ID {
@@ -97,7 +96,7 @@ func (g *Grid) Delete(id int64) error {
 		g.mtx.RUnlock()
 		return errors.New("id does not exist")
 	}
-	xb, yb := calculateBucket(point.X, point.Y, g.rows, g.columns)
+	xb, yb := calculateBucket(point.X, point.Y, int64(len(g.buckets)))
 	for i := range g.buckets[xb][yb] {
 		if g.buckets[xb][yb][i].ID == id {
 			g.buckets[xb][yb] = append(g.buckets[xb][yb][:i],
@@ -136,12 +135,12 @@ const (
 )
 
 const (
-	valid = iota
+	isValid = iota
 	tooLow
 	tooHigh
 )
 
-func adjustBucket(side, xb, yb, distance, rows, columns int64) (int64, int64, int64) {
+func adjustBucket(side, xb, yb, distance, diameter int64) (int64, int64, int64) {
 	switch side {
 	case center:
 		// do nothing
@@ -171,43 +170,144 @@ func adjustBucket(side, xb, yb, distance, rows, columns int64) (int64, int64, in
 	if xb < 0 || yb < 0 {
 		return math.MinInt64, math.MinInt64, tooLow
 	}
-	if xb >= columns || yb >= rows {
+	if xb >= diameter || yb >= diameter {
 		return math.MaxInt64, math.MaxInt64, tooHigh
 	}
-	return xb, yb, valid
+	return xb, yb, isValid
 }
 
-func (g *Grid) ClosestNeighbor(x, y int64) (Point, error) {
-	g.mtx.RLock()
+func (g *Grid) getClosestPoint(originPoint *Point) *Point {
 	var (
+		bestPoint, otherPoint      *Point
 		xb, yb, side, state        int64
-		point, bestPoint           *Point
 		hypotenuse, bestHypotenuse float64
 	)
-	xbStart, ybStart := calculateBucket(x, y, g.rows, g.columns)
-	for distance := int64(1); distance < math.MaxInt64; distance++ {
+	xbStart, ybStart := calculateBucket(originPoint.X, originPoint.Y, int64(len(g.buckets)))
+	for distance := int64(1); distance < int64(len(g.buckets)); distance++ {
 		for side = 0; side < 9; side++ {
 			if side == 0 && distance != 1 {
 				continue
 			}
-			xb, yb, state = adjustBucket(side, xbStart, ybStart, distance, g.rows, g.columns)
-			if state != valid {
+			xb, yb, state = adjustBucket(side, xbStart, ybStart, distance, int64(len(g.buckets)))
+			if state != isValid {
 				continue
 			}
 			for i := range g.buckets[xb][yb] {
-				point = &g.buckets[xb][yb][i]
-				hypotenuse = math.Hypot(float64(x-point.X), float64(y-point.Y))
+				otherPoint = &g.buckets[xb][yb][i]
+				if otherPoint.ID == originPoint.ID {
+					continue
+				}
+				hypotenuse = math.Hypot(float64(originPoint.X-otherPoint.X),
+					float64(originPoint.Y-otherPoint.Y))
 				if hypotenuse < bestHypotenuse || bestHypotenuse == 0 {
 					bestHypotenuse = hypotenuse
-					bestPoint = point
+					bestPoint = otherPoint
 				}
 			}
 		}
 		if bestPoint != nil {
-			g.mtx.RUnlock()
-			return *bestPoint, nil
+			break
 		}
 	}
+	return bestPoint
+}
+
+func insertPointIntoOrderedList(p *Point, h float64, bestPoints []Point, bestHypotenuses []float64) ([]Point, []float64) {
+	for i, bestPoint := range bestPoints {
+		if bestPoint.ID == -1 {
+			bestPoints[i] = *p
+			bestHypotenuses[i] = h
+			return bestPoints, bestHypotenuses
+		}
+		if h < bestHypotenuses[i] {
+			if bestPoints[len(bestPoints)-1].ID != -1 {
+				bestPoints = append(bestPoints, Point{ID: -1})
+				bestHypotenuses = append(bestHypotenuses, 0)
+			}
+			copy(bestPoints[i+1:], bestPoints[i:])
+			bestPoints[i] = *p
+			copy(bestHypotenuses[i+1:], bestHypotenuses[i:])
+			bestHypotenuses[i] = h
+			return bestPoints, bestHypotenuses
+		}
+	}
+	return bestPoints, bestHypotenuses
+}
+
+func (g *Grid) getClosestPoints(originPoint *Point, number int64) []Point {
+	var (
+		otherPoint          *Point
+		xb, yb, side, state int64
+		hypotenuse          float64
+	)
+	bestPoints := make([]Point, number)
+	for _, val := range bestPoints {
+		val.ID = -1
+	}
+	bestHypotenuses := make([]float64, number)
+	xbStart, ybStart := calculateBucket(originPoint.X, originPoint.Y, int64(len(g.buckets)))
+	for distance := int64(1); distance < int64(len(g.buckets)); distance++ {
+		for side = 0; side < 9; side++ {
+			if side == 0 && distance != 1 {
+				continue
+			}
+			xb, yb, state = adjustBucket(side, xbStart, ybStart, distance, int64(len(g.buckets)))
+			if state != isValid {
+				continue
+			}
+			for i := range g.buckets[xb][yb] {
+				otherPoint = &g.buckets[xb][yb][i]
+				if otherPoint.ID == originPoint.ID {
+					continue
+				}
+				hypotenuse = math.Hypot(float64(originPoint.X-otherPoint.X),
+					float64(originPoint.Y-otherPoint.Y))
+				insertPointIntoOrderedList(otherPoint, hypotenuse, bestPoints, bestHypotenuses)
+			}
+		}
+		if bestPoints[number-1].ID != -1 {
+			break
+		}
+	}
+	return bestPoints
+}
+
+func (g *Grid) ClosestPoint(x, y int64) (Point, error) {
+	g.mtx.RLock()
+	point := g.getClosestPoint(&Point{-1, x, y})
 	g.mtx.RUnlock()
-	return Point{}, errors.New("nothing found")
+	if point != nil {
+		return *point, nil
+	}
+	return Point{ID: -1}, errors.New("nothing found")
+}
+
+func (g *Grid) NearestNeighbor(id int64) (Point, error) {
+	g.mtx.RLock()
+	point, exists := g.allPoints[id]
+	if !exists {
+		g.mtx.RUnlock()
+		return Point{ID: -1}, errors.New("id does not exist")
+	}
+	point = g.getClosestPoint(point)
+	g.mtx.RUnlock()
+	if point != nil {
+		return *point, nil
+	}
+	return Point{ID: -1}, errors.New("nothing found")
+}
+
+func (g *Grid) NearestNeighbors(id, num int64) ([]Point, error) {
+	g.mtx.RLock()
+	point, exists := g.allPoints[id]
+	if !exists {
+		g.mtx.RUnlock()
+		return []Point{}, errors.New("id does not exist")
+	}
+	points := g.getClosestPoints(point, num)
+	g.mtx.RUnlock()
+	if len(points) != 0 {
+		return points, nil
+	}
+	return points, errors.New("nothing found")
 }
--- grid_test.go
+++ grid_test.go
@@ -1,10 +1,16 @@
 package spatialindex
 
-import "testing"
-import "math"
+import (
+	"math"
+	"testing"
+)
+
+const (
+	oneMillion = 1000000
+)
 
 func TestGrid(t *testing.T) {
-	grid := NewGrid(1000, 1000)
+	grid := NewGrid(oneMillion)
 	grid.Add(0, 123, 123)
 	grid.Add(1, 135, 135)
 	if grid.Add(1, 135, 135) == nil {
@@ -12,36 +18,37 @@ func TestGrid(t *testing.T) {
 	}
 	grid.Add(2, 123456789, 123456789)
 	grid.Add(3, 123456788, 123456788)
-	grid.ClosestNeighbor(-123456789, -123456789)
 	grid.Add(4, math.MaxInt64/2, math.MinInt64/2)
 	err := grid.Add(5, math.MaxInt64, math.MinInt64)
+	if err != nil {
+		t.Error(err)
+	}
+	err = grid.Add(6, math.MinInt64, math.MaxInt64)
 	if err != nil {
 		t.Error(err)
 	}
 }
 
 func BenchmarkGridCreation(b *testing.B) {
-	grid := NewGrid(1000, 1000)
 	for i := 0; i < b.N; i++ {
-		grid.Reset()
+		NewGrid(oneMillion)
 	}
 }
 
 func BenchmarkGridNeighbor(b *testing.B) {
-	grid := NewGrid(1000, 1000)
+	grid := NewGrid(oneMillion)
 	grid.Add(0, 123, 123)
 	grid.Add(1, 135, 135)
 	grid.Add(2, 123456789, 123456789)
 	grid.Add(3, 123456788, 123456788)
 	grid.Add(4, 23782, 18914)
-	grid.Add(5, -123456789, -123456789)
 	for i := 0; i < b.N; i++ {
-		grid.ClosestNeighbor(-123456789, -123456789)
+		grid.ClosestPoint(-math.MinInt64/2, -math.MinInt64/2)
 	}
 }
 
 func BenchmarkGridMutation(b *testing.B) {
-	grid := NewGrid(1000, 1000)
+	grid := NewGrid(oneMillion)
 	for i := int64(0); i < int64(b.N); i++ {
 		grid.Add(i, i*1000, i*1000)
 	}
